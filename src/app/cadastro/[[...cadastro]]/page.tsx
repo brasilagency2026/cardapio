4b43e4d5d88cc0016f781030898e6b62b8484847
gleveque27 <glwebagency2@gmail.com>
2026-06-04 17:12:00 -0300

    feat: ajout des pages de connexion et inscription clerk

---
+++ src/app/cadastro/[[...cadastro]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-neutral-50 p-4">
+      <SignUp path="/cadastro" routing="path" signInUrl="/entrar" />
+    </div>
+  );
+}
---
+++ src/app/entrar/[[...entrar]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-neutral-50 p-4">
+      <SignIn path="/entrar" routing="path" signUpUrl="/cadastro" />
+    </div>
+  );
+}
